cdd9d6efa6f964d74accb9faec0ed6e4bb3c6321
Tom Barrett <tbtommyb@gmail.com>
2015-06-01 23:18:08 +0100

    updated review images, tweak paper background

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,15 +1,14 @@
 $(document).ready(function(){
-  $('img.profile').on({
-    click: function(event) {
-        $('#popup').popup({
-            tooltipanchor: event.target,
-            autoopen: true,
-            color: '#fff',
-            type: 'tooltip',
-            outline: 'true',
-            vertical: 'bottom',
-            offsettop: 10
-        });
-    },
-  })
+  $('.profile').click(function() {
+    $(this).parent().siblings('.text-box').show('slow');
+  });
+  $(document).mouseup(function (e) {
+    var container = $('.text-box');
+
+    if (!container.is(e.target)
+        && container.has(e.target).length === 0)
+    {
+        container.hide();
+    }
+});
 });
